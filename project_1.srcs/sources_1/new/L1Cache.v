fcc5dc23cc3b26b2eb3683a92759ab8e70a23498
mesentman <mesentman2@gmail.com>
2026-05-20 11:48:10 -0500

    Added a new L1 Cache for the cpu

---
+++ project_1.srcs/sources_1/new/L1Cache.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/19/2026 02:12:51 PM
+// Design Name: 
+// Module Name: L1Cache
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module L1Cache(
+
+    );
+endmodule
